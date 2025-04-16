2fd571d975ad80a37cf878d1a97572364ac12298
gaevuha <gaevuha@gmail.com>
2025-04-16 19:42:06 +0300

    fix hw-11

--- src/main.js
+++ src/main.js
@@ -31,30 +31,30 @@ form.addEventListener('submit', async (e) => {
   clearGallery();  // Очистити попередні результати
   showLoader();    // Показати лоадер
 
-  try {
-    const data = await getImagesByQuery(query); // Отримання даних
-
-    if (data.hits.length === 0) {
-      iziToast.error({
-        message: 'Sorry, there are no images matching your search query. Please try again!',
-        position: 'topCenter',
-        icon: '', // Видалено іконку помилки
-      });
-      return;
-    }
+  getImagesByQuery(query)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topCenter',
+          icon: '', // Видалено іконку помилки
+        });
+        return;
+      }
 
       console.log('API data:', data);
 
       createGallery(data.hits); // Створити галерею
-      
-  } catch (error) {
-    iziToast.error({
-      message: 'An error occurred while fetching data.',
-      position: 'topCenter',
-      icon: '', // Без іконки
+    })
+    .catch(error => {
+      iziToast.error({
+        message: 'An error occurred while fetching data.',
+        position: 'topCenter',
+        icon: '', // Без іконки
+      });
+    })
+    .finally(() => {
+      hideLoader(); // Сховати лоадер незалежно від результату
+      form.reset(); // Очистити поле введення
     });
-  } finally {
-    hideLoader(); // Сховати лоадер незалежно від результату
-    form.reset(); // Очистити поле введення
-  }
 });
